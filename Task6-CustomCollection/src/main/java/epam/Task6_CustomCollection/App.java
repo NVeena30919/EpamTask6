ee3624456d5fa91a2f7586495b8fb7572db572fa
NVeena30919 <nerendlaveena@gmail.com>
2020-03-03 22:24:18 +0530

    Task 6

---
+++ Task6-CustomCollection/src/main/java/epam/Task6_CustomCollection/App.java
@@ -0,0 +1,13 @@
+package epam.Task6_CustomCollection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
